d4b4c1615f63267d7f64ed4afb74317287f65a9d
UzielCarranza <uzielcarranzac97@gmail.com>
2023-04-18 21:06:40 -0400

    defines props types for component

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,11 @@
+type StoreItemProps = {
+    id: number,
+    name: string,
+    price: number,
+    imgUrl: string
+}
+
+export const StoreItem = ({id, name, price, imgUrl}: StoreItemProps) => {
+    return <h1>hello</h1>
+
+}
