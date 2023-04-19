4c29d6b6ecbcf3bf102b7a3569aec522058e3fb9
UzielCarranza <uzielcarranzac97@gmail.com>
2023-04-18 21:06:18 -0400

    Adds example of passing objects to props

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,8 @@
-export function Store(){
-    return <h1>Store</h1>
+import {StoreItem} from "../components/StoreItem";
+
+export function Store() {
+    const items = {id: 1, name: "item", price: 200, imgUrl: "img/png"}
+    return (
+        <StoreItem {...items}/>
+    )
 }
